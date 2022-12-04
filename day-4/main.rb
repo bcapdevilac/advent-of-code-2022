0401400033442ba1b09f4c599cb649061446f749
bcapdevi <bcapdevilacano@gmail.com>
2022-12-04 14:58:04 +0100

    Minor improvement

--- day-4/main.rb
+++ day-4/main.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
 def full_overlap?(arr1, arr2)
-  arr1_first, arr1_last = arr1.split('-')
-  arr2_first, arr2_last = arr2.split('-')
+  arr1_first, arr1_last = arr1.split('-').map(&:to_i)
+  arr2_first, arr2_last = arr2.split('-').map(&:to_i)
 
-  arr1_first.to_i <= arr2_first.to_i && arr1_last.to_i >= arr2_last.to_i
+  arr1_first <= arr2_first && arr1_last >= arr2_last
 end
 
-elf_paris = File.read('input.txt').split("\n")
-pairs_with_full_overlap = elf_paris.select do |elf_pair|
+elf_pairs = File.read('input.txt').split("\n")
+pairs_with_full_overlap = elf_pairs.select do |elf_pair|
   first, second = elf_pair.split(',')
   full_overlap?(first, second) || full_overlap?(second, first)
 end
