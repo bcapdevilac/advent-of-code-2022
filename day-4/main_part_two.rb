03a8c8279b03e4006efe042e7e29710e7bbd4f69
bcapdevi <bcapdevilacano@gmail.com>
2022-12-04 14:58:12 +0100

    Add part two

---
+++ day-4/main_part_two.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+
+def to_range(dash_range)
+  first, last = dash_range.split('-').map(&:to_i)
+  (first..last).to_a
+end
+
+def overlap?(arr1, arr2)
+  (to_range(arr1) & to_range(arr2)).any?
+end
+
+elf_pairs = File.read('input.txt').split("\n")
+pairs_with_overlap = elf_pairs.select do |elf_pair|
+  first, second = elf_pair.split(',')
+  overlap?(first, second)
+end
+
+puts "Pairs with overlap: #{pairs_with_overlap.size}"
