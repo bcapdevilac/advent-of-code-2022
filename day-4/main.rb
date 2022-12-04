8d76b580eae4cdc8a4c80a053d16944f17e08af9
bcapdevi <bcapdevilacano@gmail.com>
2022-12-04 15:22:01 +0100

    Minor refactor

--- day-4/main.rb
+++ day-4/main.rb
@@ -1,18 +1,29 @@
 #!/usr/bin/env ruby
 
-def to_range(dash_range)
-  first, last = dash_range.split('-').map(&:to_i)
-  (first..last).to_a
+class Main
+  def initialize(file:)
+    @elf_pairs = File.read(file).split("\n")
+  end
+
+  def run
+    puts "Elf pairs with full overlap: #{pairs_with_full_overlap.size}"
+  end
+
+  def to_range(dash_range)
+    first, last = dash_range.split('-').map(&:to_i)
+    (first..last).to_a
+  end
+
+  def full_overlap?(arr1, arr2)
+    (to_range(arr1) - to_range(arr2)).empty?
+  end
+
+  def pairs_with_full_overlap
+    @elf_pairs.select do |elf_pair|
+      first, second = elf_pair.split(',')
+      full_overlap?(first, second) || full_overlap?(second, first)
+    end
+  end
 end
 
-def full_overlap?(arr1, arr2)
-  (to_range(arr1) - to_range(arr2)).empty?
-end
-
-elf_pairs = File.read('input.txt').split("\n")
-pairs_with_full_overlap = elf_pairs.select do |elf_pair|
-  first, second = elf_pair.split(',')
-  full_overlap?(first, second) || full_overlap?(second, first)
-end
-
-puts "Elf pairs with full overlap: #{pairs_with_full_overlap.size}"
+Main.new(file: 'input.txt').run
--- day-4/main_part_two.rb
+++ day-4/main_part_two.rb
@@ -1,18 +1,31 @@
 #!/usr/bin/env ruby
 
-def to_range(dash_range)
-  first, last = dash_range.split('-').map(&:to_i)
-  (first..last).to_a
+class Main
+  def initialize(file:)
+    @elf_pairs = File.read('input.txt').split("\n")
+  end
+
+  def run
+    puts "Pairs with overlap: #{pairs_with_overlap.size}"
+  end
+
+  private
+
+  def to_range(dash_range)
+    first, last = dash_range.split('-').map(&:to_i)
+    (first..last).to_a
+  end
+
+  def overlap?(arr1, arr2)
+    (to_range(arr1) & to_range(arr2)).any?
+  end
+
+  def pairs_with_overlap
+    @elf_pairs.select do |elf_pair|
+      first, second = elf_pair.split(',')
+      overlap?(first, second)
+    end
+  end
 end
 
-def overlap?(arr1, arr2)
-  (to_range(arr1) & to_range(arr2)).any?
-end
-
-elf_pairs = File.read('input.txt').split("\n")
-pairs_with_overlap = elf_pairs.select do |elf_pair|
-  first, second = elf_pair.split(',')
-  overlap?(first, second)
-end
-
-puts "Pairs with overlap: #{pairs_with_overlap.size}"
+Main.new(file: 'input.txt').run
